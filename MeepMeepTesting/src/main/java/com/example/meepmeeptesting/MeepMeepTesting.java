48210dc420cb377bb73ef65050c21a2d6f53fd6e
Emily VanCleave <evancleave8@gmail.com>
2025-09-22 14:40:57 -0500

    roadrunner!

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -2,6 +2,8 @@ package com.example.meepmeeptesting;
 
 import com.acmerobotics.roadrunner.Pose2d;
 import com.noahbres.meepmeep.MeepMeep;
+import com.noahbres.meepmeep.core.colorscheme.scheme.ColorSchemeBlueDark;
+import com.noahbres.meepmeep.core.colorscheme.scheme.ColorSchemeRedDark;
 import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
 import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
 
@@ -9,12 +11,13 @@ public class MeepMeepTesting {
     public static void main(String[] args) {
         MeepMeep meepMeep = new MeepMeep(800);
 
-        RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
-                // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
+        RoadRunnerBotEntity myFirstBot = new DefaultBotBuilder(meepMeep)
+                // We set this bot to be blue
+                .setColorScheme(new ColorSchemeBlueDark())
                 .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
                 .build();
 
-        myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(0, 0, 0))
+        myFirstBot.runAction(myFirstBot.getDrive().actionBuilder(new Pose2d(0, 0, 0))
                 .lineToX(30)
                 .turn(Math.toRadians(90))
                 .lineToY(30)
@@ -25,10 +28,11 @@ public class MeepMeepTesting {
                 .turn(Math.toRadians(90))
                 .build());
 
-        meepMeep.setBackground(MeepMeep.Background.FIELD_POWERPLAY_OFFICIAL)
+        meepMeep.setBackground(MeepMeep.Background.FIELD_DECODE_JUICE_DARK)
                 .setDarkMode(true)
                 .setBackgroundAlpha(0.95f)
-                .addEntity(myBot)
+                // Add both of our declared bot entities
+                .addEntity(myFirstBot)
                 .start();
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Decode/Auto/RoadrunnerTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Decode/Auto/RoadrunnerTest.java
@@ -1,23 +1,39 @@
 package org.firstinspires.ftc.teamcode.Decode.Auto;
 
-import androidx.annotation.NonNull;
-
-// RR-specific imports
 import com.acmerobotics.dashboard.config.Config;
-import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
 import com.acmerobotics.roadrunner.Action;
 import com.acmerobotics.roadrunner.Pose2d;
-import com.acmerobotics.roadrunner.SequentialAction;
 import com.acmerobotics.roadrunner.Vector2d;
 import com.acmerobotics.roadrunner.ftc.Actions;
-
-// Non-RR imports
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.hardware.HardwareMap;
-import com.qualcomm.robotcore.hardware.Servo;
-import com.qualcomm.robotcore.hardware.DcMotorEx;
+
 import org.firstinspires.ftc.teamcode.MecanumDrive;
 
-public class RoadrunnerTest {
+@Config
+@Autonomous(name = "RoadrunnerTest", group = "Autonomous")
+public class RoadrunnerTest extends LinearOpMode {
+
+    @Override
+    public void runOpMode() {
+
+        // Starting pose
+        Pose2d initialPose = new Pose2d(11.8, 61.7, Math.toRadians(90));
+        MecanumDrive drive = new MecanumDrive(hardwareMap, initialPose);
+
+        // Build Road Runner Core action
+        Action trajectory = drive.actionBuilder(initialPose)
+                .splineTo(new Vector2d(-43.29, 41.16), Math.toRadians(177.70))
+                .splineTo(new Vector2d(-53.71, -42.22), Math.toRadians(-88.19))
+                .splineTo(new Vector2d(49.03, -40.52), Math.toRadians(0.95))
+                .splineTo(new Vector2d(48.60, 10.32), Math.toRadians(90.48))
+                .build();
+
+
+        waitForStart();
+
+        if (opModeIsActive()) {
+            Actions.runBlocking(trajectory);
+        }
+    }
 }
