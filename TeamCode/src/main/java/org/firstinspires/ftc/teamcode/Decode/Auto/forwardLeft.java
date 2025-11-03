379c7d18d9b86de6737f22b2b249d69a0e4718d6
Emily VanCleave <evancleave8@gmail.com>
2025-11-03 14:23:58 -0600

    auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Decode/Auto/forwardLeft.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Decode/Auto/forwardLeft.java
@@ -5,7 +5,7 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
 
-@Autonomous(name = "forwardLeft", preselectTeleOp = "fullDrive")
+@Autonomous(name = "forwardLeft", group = "z", preselectTeleOp = "fullDrive")
 public class forwardLeft extends LinearOpMode {
 
     private DcMotor FrontLeft;
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Decode/Auto/forwardRight.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Decode/Auto/forwardRight.java
@@ -5,7 +5,7 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
 
-@Autonomous(name = "forwardRight", preselectTeleOp = "fullDrive")
+@Autonomous(name = "forwardRight", group = "z", preselectTeleOp = "fullDrive")
 public class forwardRight extends LinearOpMode {
 
     private DcMotor FrontLeft;
